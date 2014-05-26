5062995bf46931b77eb0e33b3fa30d4c70a88afa
Mattis Weiler <mattis@nameless-crew.de>
2014-05-26 15:02:42 +0200

    Fifth commit | Users

--- client/views/application/layout.js
+++ client/views/application/layout.js
@@ -1,8 +1,23 @@
-Template.layout.helpers({
+Template.header.helpers({
 	pageTitle: function() { return Session.get('pageTitle'); }
 });
 
 
+Deps.autorun( function() {
+  Session.set('pageTitle', 'Microscope');
+});
+
+/* ---------------------------
+
 Deps.autorun( function() { 
   alert('Value is: ' + Session.get('pageTitle')); 
 });
+
+/* ---------------------------
+
+Deps.autorun(function() {
+  var x = prompt('Set PageTitle');
+  Session.set('pageTitle', x);
+});
+
+----------------------------- */ 
