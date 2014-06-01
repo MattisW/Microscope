42f28246a0e85f01c7aeb279a348795b1bb5fee4
Mattis Weiler <mattis@nameless-crew.de>
2014-06-01 18:12:37 +0200

    Chapter 14 | Animations

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -3,3 +3,13 @@
 		return Posts.find({}, {sort: {submitted: -1}});
 	}
 }); */
+
+Template.postsList.helpers({
+  postsWithRank: function() {
+    this.posts.rewind();
+  	return this.posts.map(function(post, index, cursor) {
+  	  post._rank = index;
+  	  return post;
+  	});
+  }
+});
